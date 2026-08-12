51a9b017abef1fac063bebf20a885a940e17b4de
Cryptic1123 <demarsemerson@gmail.com>
2026-08-12 08:52:33 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const bcrypt = require("bcryptjs");
 const crypto = require("crypto");
-require("dotenv").config();
+//require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
